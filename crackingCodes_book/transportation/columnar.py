202fd52998cf50257ca9a986a5b6a1bdeddc982b
Neil Anderson <nanderson425@qub.ac.uk>
2020-02-09 21:45:19 +0000

    Added decypt

--- crackingCodes_book/transportation/columnar.py
+++ crackingCodes_book/transportation/columnar.py
@@ -1,13 +1,31 @@
-message = "This is a secret!"
+import math
 
-key = 5
-
-def encypt(text, key):
-    output = ""
+def encrypt(text, key):
+    output = []
     for num in range(key):
-        output += text[num::key]
-    return output
+        output.append(text[num::key])
+    return ''.join(output)
 
-print(message)
-encrypted = encypt(message, key)
-print(encrypted)
+def decrypt(text, key):
+    num_columns = math.ceil(len(text) / key)
+    blanks = (num_columns * key) - len(text)
+    output = [''] * num_columns
+
+    column = row = 0
+    for symbol in text:
+        output[column] += symbol
+        column += 1
+        if (column == num_columns or (column == num_columns - 1 and row >= key - blanks)):
+            column = 0
+            row += 1
+    return ''.join(output)
+
+message = "Common sense is not so common."
+
+key = 8
+
+print(f"Original: '{message}'")
+encrypted = encrypt(message, key)
+print(f"Encypted: '{encrypted}'")
+decrypted = decrypt(encrypted, key)
+print(f"Decypted: '{decrypted}'")
