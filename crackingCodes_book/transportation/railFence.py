18c8f38e4d6524206365914d5c6f9583a879a88c
Neil Anderson <nanderson425@qub.ac.uk>
2020-02-12 21:08:54 +0000

    Renaming variables and slightly restructuring

--- crackingCodes_book/transportation/railFence.py
+++ crackingCodes_book/transportation/railFence.py
@@ -1,19 +1,21 @@
 from itertools import cycle
 
-def rail_pattern(n):
-    r = list(range(n))
-    return cycle(r + r[-2:0:-1])
+def rail_pattern(key):
+    values = [num for num in range(key)]
+    return cycle(values + values[-2:0:-1])
 
 def encrypt(text, key):
-    p = rail_pattern(key)
-    return ''.join(sorted(text, key=lambda i: next(p)))
+    pattern = rail_pattern(key)
+    return ''.join(sorted(text, key=lambda i: next(pattern)))
 
 def decrypt(text, key):
-    p = rail_pattern(key)
-    indexes = sorted(range(len(text)), key=lambda i: next(p))
+    pattern = rail_pattern(key)
+    order = sorted(range(len(text)), key=lambda i: next(pattern))
     output = [''] * len(text)
-    for i, c in zip(indexes, text):
-        output[i] = c
+
+    for num, char in zip(order, text):
+        output[num] = char
+
     return ''.join(output)
 
 message = "Common sense is not so common."
