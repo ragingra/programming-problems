97a6b421d4e511c2aab714bb11745a56a408c15e
Neil Anderson <nanderson425@qub.ac.uk>
2020-02-14 17:50:54 +0000

    Finished generation of curve

--- random/hilbertCurve.py
+++ random/hilbertCurve.py
@@ -1,14 +1,14 @@
 import pygame, sys
 from pygame.locals import *
 
-order = 2
+order = 4
 n = int(pow(2, order))
 total = n * n
 
 path = ['' for num in range(total)]
 
 pygame.init()
-DISPLAY=pygame.display.set_mode((512,512),0,32)
+DISPLAY=pygame.display.set_mode((768,768),0,32)
 BLACK=(0,0,0)
 DISPLAY.fill(BLACK)
 
@@ -20,31 +20,33 @@ w, h = pygame.display.get_surface().get_size()
 
 def hilbert(i):
     points = [(0,0), (0,1), (1,1), (1,0)]
-    index = i & 3
 
-    v = points[index]
-
-    i = i >> 2
     
     index = i & 3
+    v = points[index]
 
-    x,y = v
+    for num in range(1, order):
+        num = int(pow(2, num))
+        i = i >> 2
+        index = i & 3
 
-    if index == 0:
-        pass
-    elif index == 1:
-        v = (x, y+order)
-    elif index == 2:
-        v = (x+order, y+order)
-    elif index == 3:
-        v = (x+order, y)
+        x,y = v
 
+        if index == 0:
+            v = y,x
+        elif index == 1:
+            v = (x, y+num)
+        elif index == 2:
+            v = (x+num, y+num)
+        elif index == 3:
+            y,x = num-1-x,num-1-y
+            v = (x+num, y)
+        
     return v
 
 
 for i in range(total):
     path[i] = hilbert(i)
-    print(path[i])
     scale = w // n
     path[i] = tuple([scale * num for num in path[i]])
     path[i] = tuple([num + (scale/2) for num in path[i]])
